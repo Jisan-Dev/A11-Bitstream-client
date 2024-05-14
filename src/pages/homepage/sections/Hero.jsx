bc9125af63414cd72f29f9d362207795a146b217
Jisun696 <istiakkashem35@gmail.com>
2024-05-14 22:50:47 +0600

    installed and used framer motion

--- src/pages/homepage/sections/Hero.jsx
+++ src/pages/homepage/sections/Hero.jsx
@@ -1,14 +1,27 @@
 import { Button } from '@/components/ui/button';
+import { motion } from 'framer-motion';
 
 const Hero = () => {
   return (
     <section className="flex items-center min-h-[calc(100vh-68px)] justify-center">
       <div className="mx-auto max-w-[63rem]">
         <div className="text-center">
-          <p className="text-base md:text-xl font-semibold leading-8 text-lime-600/75">Blogging at the Forefront of Innovation</p>
-          <h1 className="mt-3 text-[3rem] sm:text-[4rem] xl:text-[5rem] font-semibold leading-[3rem] md:leading-[4.5rem] tracking-tight text-black">
+          <motion.p
+            variants={{ hidden: { opacity: 0, y: -75 }, visible: { opacity: 1, y: 0 } }}
+            initial="hidden"
+            animate="visible"
+            transition={{ duration: 0.7, delay: 0.3 }}
+            className="text-base md:text-xl font-semibold leading-8 text-lime-600/75">
+            Blogging at the Forefront of Innovation
+          </motion.p>
+          <motion.h1
+            variants={{ hidden: { opacity: 0, y: 75 }, visible: { opacity: 1, y: 0 } }}
+            initial="hidden"
+            animate="visible"
+            transition={{ duration: 0.7, delay: 0.3 }}
+            className="mt-3 text-[3rem] sm:text-[4rem] xl:text-[5rem] font-semibold leading-[3rem] md:leading-[4.5rem] tracking-tight text-black">
             Bitsream: The Digital Frontier for Tech Thought Leaders
-          </h1>
+          </motion.h1>
           <p className="mt-3 text-base md:text-xl font-medium leading-normal text-slate-400">
             Unleash your passion, share your expertise, and immerse yourself in a vibrant community of tech enthusiasts from around the world.
           </p>
